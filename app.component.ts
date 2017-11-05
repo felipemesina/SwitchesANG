7b85001a43561c7994f8ed4a49f29c2d89e4d2ee
Felipe <felipejmesina@gmail.com>
2017-11-05 12:41:13 -0800

    completed

---
+++ app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Switchboard';
+  switches = [true, true, true, true, true, true, true, true, true, true];
+  OnOffSwitch(idx){
+    this.switches[idx] = !this.switches[idx];
+  }
+}
